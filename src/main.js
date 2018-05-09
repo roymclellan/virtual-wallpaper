c3e217bc58d9a4abf1d8262eb714df9f842505f7
Roy McLellan <roy_mclellan@outlook.com>
2018-05-09 18:35:38 -0500

    increment version to 0.1.3

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ const path = require('path');
 const url = require('url');
 const fs = require('fs');
 
-process.env.NODE_ENV = 'develop'
-// process.env.NODE_ENV = 'production'
+// process.env.NODE_ENV = 'develop'
+process.env.NODE_ENV = 'production'
 
 
 autoUpdater.logger = log;
